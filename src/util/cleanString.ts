d616c586becc6c67248050e624b817728fb89e1c
Dhinesh-Dhinesh <dhineshrandy953@gmail.com>
2024-07-09 19:09:38 +0530

    clear string fix

--- src/util/cleanString.ts
+++ src/util/cleanString.ts
@@ -1,6 +1,6 @@
 import { CardData } from "./FirestoreService";
 
 export function cleanString(str: string): keyof CardData {
-    // Remove special characters, spaces, and numbers, then convert to lowercase
-    return str.replace(/[^a-zA-Z]/g, '').toLowerCase() as keyof CardData;
+    // Remove special characters and spaces, then convert to lowercase
+    return str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase() as keyof CardData;
 }
